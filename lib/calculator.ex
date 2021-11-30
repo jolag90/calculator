2e430b7234facc5bd3c0d42553750a91a61307a2
Andi Altendorfer <andreas@altendorfer.at>
2021-11-30 23:08:23 +0100

    POC for the Calculator struct

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,138 +1,69 @@
 defmodule Calculator do
   use Debug, debug: false
 
-  def sum([]), do: 0
+  defstruct display: "Welcome",
+            register: 0,
+            operator: :idle,
+            input: "",
+            state: :input
 
-  def sum([n | rest]) do
-    (n + sum(rest))
-    |> debug()
+  def init() do
+    {:ok, %__MODULE__{}}
   end
 
-  def sub([]), do: 0
-
-  def sub([n | rest]) do
-    (n - sub(rest))
-    |> debug()
+  def key(
+        %__MODULE__{state: :input, input: input, display: display} = cal,
+        num_key
+      )
+      when num_key in ~w/0 1 2 3 4 5 6 7 8 9/ do
+    {:ok,
+     %{
+       cal
+       | input: "#{String.replace(input, "+", "")}#{num_key}",
+         display: "#{display |> String.replace("Welcome", "")}#{num_key}"
+     }}
   end
 
-  def mult([]), do: 1
-
-  def mult([n | rest]) do
-    (n * mult(rest))
-    |> debug()
+  def key(
+        %__MODULE__{state: :input, input: input, display: display} = cal,
+        operator_key
+      )
+      when operator_key == "+" do
+    {:ok,
+     %{
+       cal
+       | input: "+",
+         register: String.to_integer(input),
+         operator: :add,
+         display: "#{display |> String.replace("Welcome", "")} + "
+     }}
   end
 
-  def div([]), do: 1
+  def key(
+        %__MODULE__{
+          state: :input,
+          operator: :add,
+          register: register,
+          input: input
+        } = cal,
+        operator_key
+      )
+      when operator_key == "=" do
+    result = register + String.to_integer(input)
 
-  def div([n | rest]) do
-    (n / div(rest))
-    |> debug()
+    {:ok,
+     %{
+       cal
+       | input: "=",
+         register: result,
+         operator: :idle,
+         state: :input,
+         display: "#{result}"
+     }}
   end
 
-  def square(n), do: (n * n) |> debug()
-
-  def reciprocal(n) when n != 0, do: (1 / n) |> debug()
-
-  def reciprocal(_) do
-    IO.puts("divide by zero is not defined")
-  end
-
-  #######################################################################
-  def potentiate(_, 0), do: 1
-
-  def potentiate(n, x) when x > 0 do
-    potentiate_calculation(n, n, x)
-  end
-
-  def potentiate(n, x) when x < 0 do
-    potentiate_calculation_negative(1, n, x)
-  end
-
-  # _____________________________________________________________________
-
-  defp potentiate_calculation(k, _, x) when x <= 1 do
-    k |> debug()
-  end
-
-  defp potentiate_calculation(k, n, x) do
-    potentiate_calculation(k * n, n, x - 1)
-  end
-
-  defp potentiate_calculation_negative(k, _, x) when x > -1 do
-    k |> debug()
-  end
-
-  defp potentiate_calculation_negative(k, n, x) do
-    potentiate_calculation_negative(k / n, n, x + 1)
-  end
-
-  #######################################################################
-  def facultation([]), do: 1
-
-  def facultation(n) do
-    facultation_calculation(1, n)
-  end
-
-  # _____________________________________________________________________
-  defp facultation_calculation(f, n) when n <= 1, do: f |> debug()
-
-  defp facultation_calculation(f, n) do
-    facultation_calculation(f * n, n - 1)
-  end
-
-  #######################################################################
-  def prime_number?(n) do
-    prime_number_calculation(2, n)
-  end
-
-  # _____________________________________________________________________
-  defp prime_number_calculation(divisor, n) when divisor <= n / 2 do
-    dividable_by_divisor(n, divisor)
-  end
-
-  defp prime_number_calculation(_, _) do
-    true |> debug()
-  end
-
-  defp dividable_by_divisor(n, divisor) when rem(n, divisor) == 0 do
-    false |> debug()
-  end
-
-  defp dividable_by_divisor(n, divisor) do
-    prime_number_calculation(divisor + 1, n)
-  end
-
-  #######################################################################
-  @doc """
-  check if 'n' of power_of_ten is smaller, greater or equal to 0
-  if greater then multiply 1 by 10 'n' times
-  if smaller then divide 1 by 10 'n' times
-  if 'n' == 0 commit 1
-  """
-  def power_of_ten(n) when n < 0 do
-    power_of_ten_by_negative_n(1, n)
-  end
-
-  def power_of_ten(n) when n > 0 do
-    power_of_ten_by_positive_n(1, n)
-  end
-
-  def power_of_ten(0), do: 1
-  # _____________________________________________________________________
-
-  defp power_of_ten_by_negative_n(k, n) when n < 0 do
-    power_of_ten_by_negative_n(k / 10, n + 1)
-  end
-
-  defp power_of_ten_by_negative_n(k, _) do
-    k
-  end
-
-  defp power_of_ten_by_positive_n(k, n) when n > 0 do
-    power_of_ten_by_positive_n(k * 10, n - 1)
-  end
-
-  defp power_of_ten_by_positive_n(k, _) do
-    k
+  def key(%__MODULE__{} = cal, key) do
+    IO.puts("Invalid input '#{key}'")
+    {:ok, cal}
   end
 end
