ff5c34139ad007885a5ec0b3e83a5013e1b78a8f
Andi Altendorfer <andreas@altendorfer.at>
2021-12-20 17:14:02 +0100

    Change CLI to use GenServer

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,28 +1,24 @@
-# defmodule Calculator.CLI do
-#  def main(_args) do
-#    IO.puts("THE CALCULATOR")
-#    # init cal
-#    Calculator.init()
-#    |> loop()
-#
-#    # enter loop
-#
-#    # done
-#  end
-#
-#  defp loop({:ok, cal}) do
-#    cal
-#    |> Calculator.key(String.trim(IO.gets("")))
-#    |> print()
-#    |> loop()
-#  end
-#
-#  defp loop({:quit, _cal}), do: IO.puts("thanks")
-#
-#  defp print({_, cal} = state) do
-#    IO.puts(cal.display)
-#    state
-#  end
-# end
-#
-## IO.gets(tastn & enter) |>String.trim(),.... noch schaun wia ma einzelnen tastn druck lesn ko
+defmodule Calculator.CLI do
+  def main(_args) do
+    IO.puts("THE CALCULATOR")
+    # init cal
+    Calculator.start_link()
+    |> loop()
+  end
+
+  defp loop({:ok, cal}) do
+    print(cal)
+    key = String.trim(IO.gets(""))
+
+    unless key == "q" do
+      Calculator.press_key(cal, key)
+      loop({:ok, cal})
+    end
+  end
+
+  defp print(cal) do
+    IO.puts(Calculator.display(cal))
+  end
+end
+
+# IO.gets(tastn & enter) |>String.trim(),.... noch schaun wia ma einzelnen tastn druck lesn ko
